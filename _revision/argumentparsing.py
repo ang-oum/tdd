dfd38a555bf46a65b7593b25395b82420381ec04
Angelos <89702737+ang-oum@users.noreply.github.com>
2023-10-03 19:55:09 +0200

    .

---
+++ _revision/argumentparsing.py
@@ -0,0 +1,27 @@
+
+#   https://www.youtube.com/watch?v=alSgAbyC7K8
+
+import sys
+import getopt
+
+opts, args = getopt.getopt(sys.argv[1:], "f:m", ['filename', 'message'])
+
+
+
+
+
+
+
+
+
+def myfunction(*args, **kwargs):
+    print(args[0])
+    print(args[1])
+    print(args[2])
+    print(args[3])
+    print(kwargs['KEYONE'])
+    print(kwargs['KEYTWO'])
+ 
+myfunction('hey', True,19,'wow', KEYONE="TEST", KEYTWO=7)
+
+
