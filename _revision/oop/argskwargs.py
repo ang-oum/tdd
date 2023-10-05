599c85d5b0ce58a2bc1cd24b3a89410488b9704a
Angelos <89702737+ang-oum@users.noreply.github.com>
2023-10-05 15:38:26 +0200

    .

---
+++ _revision/oop/argskwargs.py
@@ -0,0 +1,40 @@
+
+
+# defining car class
+class car():
+    # args receives unlimited no. of arguments as an array
+    def __init__(self, *args):
+        # access args index like array does
+        self.speed = args[0]
+        self.color = args[1]
+ 
+ 
+# creating objects of car class
+audi = car(200, 'red')
+bmw = car(250, 'black')
+mb = car(190, 'white')
+ 
+# printing the color and speed of the cars
+print(audi.color)
+print(bmw.speed)
+
+
+
+# defining car class
+class car():
+    # args receives unlimited no. of arguments as an array
+    def __init__(self, **kwargs):
+        # access args index like array does
+        self.speed = kwargs['s']
+        self.color = kwargs['c']
+ 
+ 
+# creating objects of car class
+audi = car(s=200, c='red')
+bmw = car(s=250, c='black')
+mb = car(s=190, c='white')
+ 
+# printing the color and speed of cars
+print(audi.color)
+print(bmw.speed)
+
